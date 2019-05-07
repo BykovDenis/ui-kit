2a81be34069ba7ece2c9bba9e7508634eed41a89
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2019-05-07 19:19:49 +0300

    fixed

--- packages/react-ui-components/src/App.js
+++ packages/react-ui-components/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import CustomButton from '@riski-react-ui-components/custom-button';
 import CustomButton from 'riski-react-ui-components/custom-button';
+import DatePickerCustom from 'riski-react-ui-components/date-picker-custom';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <CustomButton>First custom component</CustomButton>
+          <DatePickerCustom value={'2019.04.20'} />
         </header>
       </div>
     );
