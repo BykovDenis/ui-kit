b0a8c630513cc377fdc91f1779434d989b369fca
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2019-05-07 18:08:44 +0300

    fixed

--- packages/react-ui-components/src/App.js
+++ packages/react-ui-components/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import CustomButton from 'riski-react-ui-components/packages/custom-button';
+import CustomButton from '@riski-react-ui-components/custom-button';
 import './App.css';
 
 class App extends Component {
