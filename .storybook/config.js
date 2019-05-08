39217d9ec13c62ee0ec2b3e9ac89c8a250c1af01
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2019-05-08 13:26:31 +0300

    fixed

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,20 @@
 import { configure } from '@storybook/react';
+import { ThemeProvider } from '../src/theme-provider';
+
+// addon-info
+setDefaults({
+  inline: true,
+  source: false,
+  TableComponent
+});
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../packages', true, /.stories.js$/);
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
-}
+
+const ThemeProviderDecorator = storyFn => (
+  <ThemeProvider>{storyFn()}</ThemeProvider>
+);
+
+addDecorator(ThemeProviderDecorator);
 
 configure(loadStories, module);
---
+++ src/theme-provider.js
@@ -0,0 +1,18 @@
+// src/ThemeProvider.jsx
+
+import React from 'react';
+import { MomentUtils, MuiPickersUtilsProvider } from 'material-ui-pickers'  // 'material-ui-pickers/utils/date-fns-utils';
+import moment from 'moment-timezone';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
+
+import themeCustom from '../themes';
+
+const theme = createMuiTheme(themeCustom);
+
+const ThemeProvider = ({ children }) => (
+  <MuiPickersUtilsProvider utils={MomentUtils} moment={moment}>
+    <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>
+  </MuiPickersUtilsProvider>
+);
+
+export default ThemeProvider;
