d2b1f8dcaa886b1fc2e55168ceafd0a6672537e3
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2020-01-13 19:12:56 +0300

    demo components created

---
+++ demo-app/src/components/app/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/Tablebody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import ReactSelectCustom from 'riski-react-ui/react-select-custom';
+import ErrorComponent from 'riski-react-ui/error';
+import NoDataAvailableComponent from 'riski-react-ui/not-data-available';
+import MuiButton from 'riski-react-ui/mui-button';
+import './index.css';
+
+function Index() {
+  return (
+    <div className="App">
+      <Typography variant="h1">Demo of components</Typography>
+      <Table>
+        <TableBody>
+          <TableRow>
+            <TableCell>
+              <ReactSelectCustom activeElement="one" elements={['one', 'two', 'three']} />
+            </TableCell>
+            <TableCell>
+              <MuiButton>Click me!</MuiButton>
+            </TableCell>
+            <TableCell>
+              <NoDataAvailableComponent />
+            </TableCell>
+            <TableCell>
+              <ErrorComponent />
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
+
+export default Index;
