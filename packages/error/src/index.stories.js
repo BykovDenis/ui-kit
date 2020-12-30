b6463dffb660f5794485e09e24e94aadfbdbf59a
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2020-12-30 11:37:32 +0300

    removed some components

--- packages/error/src/index.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import Error from '.';
-
-storiesOf('Error component', module).add('with text', () => <Error />);
--- packages/mui-button/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React, { Fragment } from 'react';
-import Button from '@material-ui/core/Button';
-
-const MuiButton = ({ children, onClick }) => <Button onClick={onClick}>{children}</Button>
-
-export default MuiButton
