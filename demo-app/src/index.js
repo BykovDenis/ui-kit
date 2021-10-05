448be53612209cd8e088826fa976ed0cd808d648
Bykov Denis Stanislavovich <bykov.d.sta@sberbank.ru>
2021-10-05 14:14:55 +0300

    version actualize
    customized and matherial removed
    
    versions updated
    
    versions updated
    
    versions updated
    
    versions updated
    
    versions updated
    
    versions updated
    
    v0.0.2
    
    LICENSE added
    
    v0.0.3
    
    npmrc edited
    
    dist for the button added
    
    npmrc removed from versioning
    
    package.json fixed

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import muiTheme from './themes';
 
 const init = () => (
-  <MuiThemeProvider theme={muiTheme}>
-    <App />
-  </MuiThemeProvider>
+  <App />
 )
 
 ReactDOM.render(init(), document.getElementById('root'));
--- packages/error/index.stories.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import Error from './src';
-
-storiesOf('Error component', module).add('with text', () => <Error />);
