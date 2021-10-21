27a25f40398f72d0847a3043d234adb829fac100
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2021-10-21 17:11:30 +0300

    public library configurated

--- demo-app/src/components/app/index.js
+++ demo-app/src/components/app/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './index.css';
+import CustomButton from '@sber-riski-cib-ui/core/custom-button'
 
 function Index() {
   return (
     <div className="App">
       <h1>Demo of the common components</h1>
-      <Table>
-        <TableBody>
-          <TableRow>
-            <TableCell>
-              <CustomButton></CustomButton>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+            <CustomButton>123</CustomButton>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
