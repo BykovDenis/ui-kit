ef2418dd3775a6e18313cb5502c583fb2badc18d
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2021-12-03 11:58:34 +0300

    the new component button added and jest added

--- packages/button/rollup.config.js
+++ packages/button/rollup.config.js
@@ -1,5 +1,16 @@
+import replace from '@rollup/plugin-replace';
+import autoprefixer from 'autoprefixer';
+import postcss from 'rollup-plugin-postcss';
 import scss from 'rollup-plugin-scss';
+import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
+
+// web design token replacements
+const tokens = {
+  __CLOCKSELECTOR__: '.clock',
+  __CLOCKINTERVAL__: 1000,
+  __CLOCKFORMAT__: 'formatHMS',
+};
 
 import pkg from './package.json';
 
@@ -12,8 +23,26 @@ export default {
       exports: 'named',
       sourcemap: true,
       strict: false,
+      entryFileNames: '[name].[hash].js',
     },
   ],
-  plugins: [typescript({ objectHashIgnoreUnknownHack: true }), scss()],
+  plugins: [
+    typescript({ objectHashIgnoreUnknownHack: false }),
+    scss({
+      output: 'dist/styles.css',
+      outputStyle: 'compressed',
+      processor: postcss({ plugins: { autoprefixer }, modules: {} }),
+    }),
+    terser(),
+    replace({
+      preventAssignment: true,
+      include: ['src/**/*'],
+      values: {
+        changed: 'replaced',
+      },
+      __buildDate__: () => JSON.stringify(new Date()),
+      __buildVersion: 15,
+    }),
+  ],
   external: ['react', 'react-dom'],
 };
--- packages/custom-button/src/index.tsx
+++ packages/custom-button/src/index.tsx
@@ -1,23 +1,23 @@
 import React, { Fragment } from 'react';
 
 interface IButton {
-  type?: 'submit' | 'reset' | 'button';
   children: any;
   onClick?: () => void;
+  type?: 'submit' | 'reset' | 'button';
 }
 
 const Button: React.FunctionComponent<IButton> = (props: IButton) => {
-
   const onButtonClick = () => {
     props?.onClick && props?.onClick();
-  }
-  return (<Fragment>
+  };
+  return (
+    <Fragment>
       <button type={props.type} className="button" onClick={onButtonClick}>
         {props.children}
       </button>
     </Fragment>
   );
-}
+};
 
 Button.defaultProps = {
   type: 'button',
