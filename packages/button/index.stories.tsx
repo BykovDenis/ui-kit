a3a39032d3597fecfa21316cc4396a6119241361
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2021-12-16 13:00:49 +0300

    the buttons variants added

--- packages/button/index.stories.tsx
+++ packages/button/index.stories.tsx
@@ -10,7 +10,11 @@ export default {
   argTypes: { onClick: { action: 'clicked' } },
 } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = () => <Button type="submit" onClick={action('clicked')}>Button</Button>;
+const Template: ComponentStory<typeof Button> = () => <Button onClick={action('clicked')}>Button</Button>;
 const Template1: ComponentStory<typeof Button> = () => <Button onClick={action('clicked')} disabled={true}>Button</Button>;
+const ButtonTextTemplate: ComponentStory<typeof Button> = () => <Button variant="text" onClick={action('clicked')}>Button</Button>;
+const ButtonOutlinedTemplate: ComponentStory<typeof Button> = () => <Button variant="outlined" onClick={action('clicked')}>Button</Button>;
 export const NormalButton = Template.bind({});
 export const DisabledButton = Template1.bind({});
+export const ButtonText = ButtonTextTemplate.bind({});
+export const ButtonOutlined = ButtonOutlinedTemplate.bind({});
--- packages/button/src/index.tsx
+++ packages/button/src/index.tsx
@@ -4,15 +4,27 @@ import styled from 'styled-components';
 import hexToRgb from '../../helpers/hex-to-rgb';
 import ThemeContext from '../../styles/src/themes';
 
+const CONTAINED: string = 'contained';
+const OUTLINED: string = 'outlined';
+
+interface IButtonVariants {
+  contained?: string;
+  outlined?: string;
+  text?: string;
+}
+
 interface IButton {
   ReactThemeContext?: any;
   backgroundColor?: string;
   children?: any;
   color?: string;
   disabled?: boolean;
+  height: number;
   onClick?: (evt: any) => void;
   theme?: any;
   type?: string;
+  variant?: IButtonVariants;
+  width: number;
 }
 
 const Button =
@@ -29,21 +41,27 @@ const Button =
       text-align: center;
       letter-spacing: 0.39998px;
       text-transform: uppercase;
-      color: ${(props: IButton) => props.color};
-      padding-top: 11px;
-      padding-bottom: 11px;
-      background-color: ${(props: IButton) => props.backgroundColor};
+      color: ${(props: IButton) => (props?.variant === CONTAINED ? props.color : props.backgroundColor)};
+      padding-top: 10px;
+      padding-bottom: 10px;
+      background-color: ${(props: IButton) => (props?.variant === CONTAINED ? props.backgroundColor : props.color)};
       cursor: pointer;    
+      border: ${(props: IButton) =>
+        props?.variant === OUTLINED ? `1px solid ${props.backgroundColor}` : '1px solid transparent'};
 
       &:hover {
-        filter: drop-shadow(0px 2px 2px rgba(0, 0, 0, 0.25));
-        background-color: ${(props: IButton) => hexToRgb(props.backgroundColor, 0.85)};
+        box-shadow: ${(props: IButton) =>
+          props?.variant === CONTAINED ? '0 2px 2px 0 rgba(0, 0, 0, 0.25)' : '0 1px 1px rgba(0, 0, 0, 0.15)'}; 
+        background-color: ${(props: IButton) =>
+          props?.variant === CONTAINED ? hexToRgb(props.backgroundColor, 0.85) : hexToRgb(props.backgroundColor, 0.05)};
         border-radius: 4px;
       }
 
       &:active {
-        filter: drop-shadow(0px 2px 2px rgba(0, 0, 0, 0.25));
-        background-color: ${(props: IButton) => hexToRgb(props.backgroundColor, 0.5)};
+        box-shadow: : ${(props: IButton) =>
+          props?.variant === CONTAINED ? '0 2px 2px 0 rgba(0, 0, 0, 0.25)' : '0 1px 1px 0 rgba(0, 0, 0, 0.15)'};
+        background-color: ${(props: IButton) =>
+          props?.variant === CONTAINED ? hexToRgb(props.backgroundColor, 0.5) : hexToRgb(props.backgroundColor, 0.25)};
         border-radius: 4px;
       }
 
@@ -59,6 +77,8 @@ export default React.memo((props: IButton) =>
     <props.ReactThemeContext.Consumer>
       {(theme: any) => (
         <Button
+          width={props.width}
+          height={props.height}
           type={props.type}
           onClick={props?.onClick}
           disabled={props?.disabled}
@@ -74,6 +94,9 @@ export default React.memo((props: IButton) =>
     <ThemeContext.Consumer>
       {(theme: any) => (
         <Button
+          variant={props?.variant ?? CONTAINED}
+          width={props?.width}
+          height={props?.height}
           type={props.type}
           color={props?.color || theme?.palette?.baseButtonFontColor}
           backgroundColor={props?.backgroundColor || theme?.palette?.primary?.main}
