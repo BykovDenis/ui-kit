0ea844f87a2358701f2a40b8aa4457ab275a50ed
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2021-12-20 10:44:00 +0300

    checkbox updated

--- packages/button/types/ibutton.ts
+++ packages/button/types/ibutton.ts
@@ -4,12 +4,12 @@ interface IButton {
   ReactThemeContext?: any;
   backgroundColor?: string;
   backgroundImage?: string;
+  baseFontSize: string;
   children?: any;
   className?: any;
   color?: string;
   dataset?: any;
   disabled?: boolean;
-  fontFamily: string;
   fontSize: string;
   height: string;
   onClick?: (evt: any) => void;
