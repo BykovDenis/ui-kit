1e3ff9fca0fe8c4a89f41a84900f833ccf61c7f4
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2022-01-12 16:57:31 +0300

    property renamed

--- packages/checkbox/types/icheckbox.d.ts
+++ packages/checkbox/types/icheckbox.d.ts
@@ -8,12 +8,12 @@ interface ICheckbox {
   fontFamily?: string;
   fontSize?: string;
   id: string;
+  indeterminate?: boolean;
   label?: string;
   name?: string;
   onChange: (evt: any) => void;
   tabIndex?: string;
   theme?: any;
-  undeterminate?: boolean;
 }
 
 export default ICheckbox;
