c4555e8fbd40616a0947a165020ecc208991c74b
Bykov Denis Stanislavovich <bykov.d.sta@sberbank.ru>
2022-01-13 16:19:44 +0300

    display edited for the label

--- packages/label/src/index.tsx
+++ packages/label/src/index.tsx
@@ -11,21 +11,21 @@ const Label: React.FunctionComponent<ILabel> = (props: ILabel) => {
     styled.label <
     ILabel >
     `
-        &:focus {
-          outline: 1px solid ${(props: ILabel) => hexToRgba(props?.focusColor, 0.3)};
-          box-shadow: 1px 1px 5px 3px ${(props: ILabel) => hexToRgba(props?.focusColor, 0.3)};
-        }
-      
-        position: relative;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: flex-start;
-        font-family: ${(props: ILabel) => props.fontFamily};
-        cursor: pointer;
-        color: ${(props: ILabel) => props.color};
-        font-size: ${(props: ILabel) => props.fontSize};
-        line-height: 1.3;      
+      position: relative;
+      display: inline-flex;
+      vertical-align: center;
+      flex-direction: row;
+      align-items: center;
+      justify-content: flex-start;
+      font-family: ${(props: ILabel) => props.fontFamily};
+      cursor: pointer;
+      color: ${(props: ILabel) => props.color};
+      font-size: ${(props: ILabel) => props.fontSize};
+      line-height: 1.3;
+      &:focus {
+        outline: 1px solid ${(props: ILabel) => hexToRgba(props?.focusColor, 0.3)};
+        box-shadow: 1px 1px 5px 3px ${(props: ILabel) => hexToRgba(props?.focusColor, 0.3)};
+      }
       `;
 
   return props.ReactThemeContext ? (
