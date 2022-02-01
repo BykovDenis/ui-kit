abb93554a99a93bad819444bd7b45cfc8b099458
Bykov Denis Stanislavovich <bykov.d.sta@sberbank.ru>
2022-02-01 17:48:37 +0300

    the button width initial

--- packages/button/src/button.styled.tsx
+++ packages/button/src/button.styled.tsx
@@ -33,8 +33,8 @@ const ButtonStyled =
     cursor: pointer;    
     border: ${(props: IButton) =>
       props?.variant === OUTLINED ? `1px solid ${props.backgroundColor}` : '1px solid transparent'};
-    width: ${(props: IButton) => props?.width ?? 'initial'}px;
-    height: ${(props: IButton) => props?.height ?? 'initial'}px;
+    width: ${(props: IButton) => `${props?.width}px` ?? 'initial'};
+    height: ${(props: IButton) => `${props?.height}px` ?? 'initial'};
 
     &:focus {
       outline: 1px solid ${(props: IButton) => rgbToRgba(props?.focusColor, 0.3)};
