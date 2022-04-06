8d6e2da988a500000c9e003c75a4025fc72c3ab9
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2022-04-06 13:11:26 +0300

    Unit tests update

--- packages/button/__tests__/button.test.tsx
+++ packages/button/__tests__/button.test.tsx
@@ -3,7 +3,7 @@ import { themes } from '../../styles/src';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-import Button from '../src/button';
+import Button from '../src';
 
 it('Button renders correctly', () => {
   const themeCustom: any = {
