94d80d272ab0389de14880e016c8de38a5434a50
Bykov Denis Stanislavovich <bykov.d.sta@sberbank.ru>
2022-04-06 10:19:13 +0300

    button and test fixed

--- packages/button/src/button1.tsx
+++ packages/button/src/button1.tsx
@@ -3,7 +3,7 @@ import ThemeContext from '../../styles/src/themes';
 import ButtonStyled from '../src/button.styled';
 
 const Button1 = (props: any) => {
-  const componentThemed = (theme: any) => <ButtonStyled {...props}>123</ButtonStyled>;
+  const componentThemed = (theme: any) => <button {...props}>123</button>;
 
   const Consumer: any = props.ReactThemeContext ? props.ReactThemeContext.Consumer : ThemeContext.Consumer;
 
