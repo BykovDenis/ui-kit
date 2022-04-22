2192e092315b075834e36514d9764078c133a47b
Bykov Denis Stanislavovich <bykov.d.sta@sberbank.ru>
2022-04-22 19:00:27 +0300

    The some control added

--- packages/select/rollup.config.dev.js
+++ packages/select/rollup.config.dev.js
@@ -4,30 +4,38 @@ import svg from 'rollup-plugin-svg';
 import typescript from 'rollup-plugin-typescript2';
 
 import pkg from './package.json';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.tsx',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true,
-      strict: false,
-    },
-  ],
-  plugins: [
-    cleaner({
-      targets: ['./dist'],
-    }),
-    typescript({ objectHashIgnoreUnknownHack: false, inlineSourceMap: true }),
-    svg(),
-    postcss({
-      autoModules: true,
-      modules: {
-        generateScopedName: '[hash:base64:8]',
+export default [
+  {
+    input: 'src/index.tsx',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true,
+        strict: false,
       },
-    }),
-  ],
-  external: ['react', 'react-dom', 'styled-components'],
-};
+    ],
+    plugins: [
+      cleaner({
+        targets: ['./dist'],
+      }),
+      typescript({ objectHashIgnoreUnknownHack: false, inlineSourceMap: true }),
+      svg(),
+      postcss({
+        autoModules: true,
+        modules: {
+          generateScopedName: '[hash:base64:8]',
+        },
+      }),
+    ],
+    external: ['react', 'react-dom', 'styled-components'],
+  },
+  {
+    input: './src/index.d.ts',
+    output: [{ file: pkg.types, format: 'es' }],
+    plugins: [dts()],
+  },
+];
