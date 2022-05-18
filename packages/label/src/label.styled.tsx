3c71e1a06bc6d38d508d7d976e5b8764c2fbf78e
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2022-05-18 13:35:37 +0300

    the background attribure added into the Label components

--- packages/label/src/label.styled.tsx
+++ packages/label/src/label.styled.tsx
@@ -21,6 +21,7 @@ const Label =
       white-space: nowrap;
       font-weight: ${(props: ILabel) => props.fontWeight}; 
       width: ${(props: ILabel) => (props.width ? `${props.width}px` : '100%')};  
+      background-color: ${(props: ILabel) => (props.backgroundColor ? props.backgroundColor : 'inherit')};
       &:focus {
         outline: 1px solid ${(props: ILabel) => rgbToRgba(props?.focusColor, 0.3)};
         box-shadow: 1px 1px 5px 3px ${(props: ILabel) => rgbToRgba(props?.focusColor, 0.3)};
