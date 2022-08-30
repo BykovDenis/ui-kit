12ca93235c89394d4dd72817d52f44c68bd4d1a7
bykov.d.sta@sberbank.ru <bykov.d.sta@sberbank.ru>
2022-08-30 11:55:47 +0300

    The new component form-control added

---
+++ packages/form-control/types/iform-control-styled.d.ts
@@ -0,0 +1,19 @@
+interface IFormControlStyled {
+  alignItems: string;
+  backgroundColor?: string;
+  color?: string;
+  flexDirection: string;
+  flexGrow?: number;
+  height?: string;
+  justifyContent: string;
+  margin?: string;
+  maxHeight?: string;
+  overflowY?: string;
+  padding?: string;
+  position?: string;
+  textAlign?: string;
+  whiteSpace?: string;
+  width?: string;
+}
+
+export default IFormControlStyled;
