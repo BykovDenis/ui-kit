a8b619224e513561c4aaa2bc1e73c381a09b78a8
bykov.d.sta@sberbank.ru <bykov.d.sta@sberbank.ru>
2023-02-02 17:14:03 +0300

    the version UI Kit updated. The type IList edited

--- packages/list/src/list-div.styled.tsx
+++ packages/list/src/list-div.styled.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+import isNotEmptyString from '../../helpers/is-not-empty-string';
 import Ilist from '../types/ilist';
 
 const ListDiv =
@@ -9,6 +10,7 @@ const ListDiv =
   display: block;
   list-style: none;
   font-family: ${(props: Ilist) => props.fontFamily};
+  margin: ${(props: Ilist) => (isNotEmptyString(props.margin) ? props.margin : 0)}; 
 `;
 
 export default ListDiv;
