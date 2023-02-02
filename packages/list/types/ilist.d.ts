9184b0a64bd0937d24186b9b42c2b0fce5f8d15a
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2023-02-02 17:14:03 +0300

    the version UI Kit updated. The type IList edited

--- packages/list/types/ilist.d.ts
+++ packages/list/types/ilist.d.ts
@@ -3,18 +3,19 @@ import React from 'react';
 import IListElement from '../../list-item/types/ilist-element';
 
 interface IList {
+  backgroundColor?: string;
   children?: any;
   className?: string;
   colorTheme?: string;
   elements?: Array<IListElement>;
   fontFamily?: string;
-  onKeyUp?: (evt?: React.KeyboardEvent<HTMLElement>, listRef?: React.Ref<any>) => void;
-  onMouseDown?: (evt?: React.MouseEvent<HTMLElement, MouseEvent>, listRef?: React.Ref<any>) => void;
-  type?: string;
-  backgroundColor?: string;
   hoverBackgroundColor?: string;
   hoverColor?: string;
+  margin?: string;
+  onKeyUp?: (evt?: React.KeyboardEvent<HTMLElement>, listRef?: React.Ref<any>) => void;
+  type?: string;
   underlineColor?: string;
+  onMouseDown?: (evt?: React.MouseEvent<HTMLElement, MouseEvent>, listRef?: React.Ref<any>) => void;
 }
 
 export default IList;
