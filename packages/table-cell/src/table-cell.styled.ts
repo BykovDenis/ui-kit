ae450e9988a23f52caf9df74f5968a345f31424d
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2023-02-28 17:59:00 +0300

    The TableCell width fixed

--- packages/table-cell/src/table-cell.styled.ts
+++ packages/table-cell/src/table-cell.styled.ts
@@ -7,7 +7,7 @@ const TableCellStyled =
   styled.td <
   TTableCell >
   `
-  width: ${(props: TTableCell) => getMeasureValue((props?.width, 'initial'))};  
+  width: ${(props: TTableCell) => getMeasureValue(props?.width, 'initial')};  
   position: ${(props: TTableCell) => props?.position ?? 'relative'};
   padding: ${(props: TTableCell) => props?.padding ?? '10px 24px'};
   border-right: ${(props: TTableCell) => (props?.borderColor ? `1px solid ${props.borderColor}` : 'none')};
