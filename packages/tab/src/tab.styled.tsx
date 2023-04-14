d3c8b0ee49c2751ebace368bbd1c3f244751b80e
bykov.d.sta@sberbank.ru <bykov.d.sta@sberbank.ru>
2023-04-14 16:43:47 +0300

    styles updated

--- packages/tab/src/tab.styled.tsx
+++ packages/tab/src/tab.styled.tsx
@@ -24,14 +24,15 @@ const Tab =
       white-space: ${(props: TTabStyled) => (props?.whiteSpace ? props.whiteSpace : 'nowrap')};
       font-weight: ${(props: TTabStyled) => props.fontWeight || 900}; 
       width: ${(props: TTabStyled) => getMeasureValue(props?.width, '100%')};
+      max-width: ${(props: TTabStyled) => getMeasureValue(props?.maxWidth, '100%')};
       height: ${(props: TTabStyled) => getMeasureValue(props?.height)};
       background-color: ${(props: TTabStyled) => (props.backgroundColor ? props.backgroundColor : 'inherit')};
       word-break: ${(props: TTabStyled) => props.wordBreak ?? 'initial'};  
       padding: ${(props: TTabStyled) => props.padding || 'initial'};
-      min-height: 40px;
       border: none;
       border-bottom: ${(props: TTabStyled) => `4px solid ${props.isActive ? props.borderColor : 'transparent'}`};
       transition: border-color 500ms ease-in-out;
+      min-height: ${(props: TTabStyled) => getMeasureValue(props.minHeight, '40px')};
   `;
 
 export default Tab;
