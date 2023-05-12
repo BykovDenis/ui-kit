f9478b4156199ef421a2b58be638abfc96a99602
bykov.d.sta@sberbank.ru <bykov.d.sta@sberbank.ru>
2023-05-12 16:28:20 +0300

    interface typography edited

--- packages/typography/types/iheader.ts
+++ packages/typography/types/iheader.ts
@@ -1,9 +1,9 @@
 interface IHeader {
   error?: boolean;
-  fontSize: number;
-  color: string;
-  backgroundColor: string;
-  fontFamily: string;
+  fontSize?: number;
+  color?: string;
+  backgroundColor?: string;
+  fontFamily?: string;
   textDecoration?: 'none' | 'inherit' | 'underline' | 'overline' | 'line-through' | 'blink';
   textTransform?: 'capitalize' | 'lowercase' | 'uppercase' | 'none' | 'inherit';
 }
