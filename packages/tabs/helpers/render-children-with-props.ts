b77c30227f77e3669b5fc717475ebb75789e950c
bykov.d.sta@sberbank.ru <bykov.d.sta@sberbank.ru>
2023-05-17 16:57:22 +0300

    The Tabs logic fixed

--- packages/tabs/helpers/render-children-with-props.ts
+++ packages/tabs/helpers/render-children-with-props.ts
@@ -2,14 +2,16 @@ import React from 'react';
 
 function renderChildren(children, props: any) {
   const propsParsed: any = { ...props };
-  if (propsParsed?.children?.length > 0) {
+  const childrenCount: number = children?.length;
+  if (childrenCount > 0) {
     delete propsParsed?.children;
   }
   return React.Children.map(children, (child: React.ReactElement, index: number) => {
     const tabName: string = child?.props?.name;
+    const isDisabled: boolean = child?.props?.disabled;
     return React.cloneElement(child, {
       ...propsParsed,
-      isActive: tabName ? props.value === tabName : props.value === index,
+      isActive: (tabName ? props.value === tabName : props.value === index) || (childrenCount === 1 && !isDisabled),
       tabActive: tabName ? tabName : index,
     });
   });
