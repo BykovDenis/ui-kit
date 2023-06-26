a0f2a43d9851847eebf2a2adc14719bb887c2239
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2023-06-26 18:44:36 +0300

    Select flag for React.StrictMode fixed

--- demo-app/src/app.tsx
+++ demo-app/src/app.tsx
@@ -1,8 +1,9 @@
 import { useContext, useState } from 'react';
 import getNewReactThemeContext from '@sber-risks-ui/styles/src';
 import { themes } from '@sber-risks-ui/styles/src/themes';
-import InputTesting from "./components/input-testing";
-import TextFieldTesting from "./components/text-field-testing";
+import InputTesting from './components/input-testing';
+import TextFieldTesting from './components/text-field-testing';
+import Select from '../../packages/select/src';
 
 function App() {
   const [dateValue, setDateValue] = useState<string>('11.05.2022');
@@ -32,8 +33,20 @@ function App() {
 
   const ReactThemeContext = getNewReactThemeContext(themes.dark);
 
-  const elementsEmpty: Array<number> = new Array(150).fill(0);
-  // const elements: Array<string> = elementsEmpty.map((element: number, index: number) => `${index}`);
+  const elementsState: Array<{ label: string; value: string }> = [
+    { label: 'Январь', value: '0' },
+    { label: 'Февраль', value: '1' },
+    { label: 'Март', value: '2' },
+    { label: 'Апрель', value: '3' },
+    { label: 'Май', value: '4' },
+    { label: 'Июнь', value: '5' },
+    { label: 'Июль', value: '6' },
+    { label: 'Август', value: '7' },
+    { label: 'Сентябрь', value: '8' },
+    { label: 'Октябрь', value: '9' },
+    { label: 'Ноябрь', value: '10' },
+    { label: 'Декабрь', value: '11' },
+  ];
 
   const context: any = useContext(ReactThemeContext);
 
@@ -42,6 +55,12 @@ function App() {
       <div style={{ background: context.mainBackgroundColor, height: '100vh', margin: 0 }}>
         <InputTesting />
         <TextFieldTesting />
+        <Select
+          id="some-select"
+          name="some-select"
+          elements={elementsState}
+          activeELement={{ label: 'Сентябрь', value: '8' }}
+        />
       </div>
     </ReactThemeContext.Provider>
   );
