2dedc5537d286b6c6041440fa34b9504e828afee
bykov.d.sta@sberbank.ru <bykov.d.sta@sberbank.ru>
2023-07-10 13:44:11 +0300

    Progressbar styles fixed

--- packages/progress-bar/src/progress-value.styled.ts
+++ packages/progress-bar/src/progress-value.styled.ts
@@ -1,16 +1,19 @@
 import styled from 'styled-components';
 import TProgressBarProgressValue from '../types/tprogress-bar-progress-value';
+import getMeasureValue from '../../helpers/get-measure-value';
 
 const ProgressValueStyled = styled.p<TProgressBarProgressValue>`
   position: absolute;
   left: 50%;
   top: 50%;
+  width: fit-content;
+  height: ${(props: TProgressBarProgressValue) => getMeasureValue(props.fontSize)};
   transform: ${(props: TProgressBarProgressValue) =>
     props.direction == 'ltr' ? 'translate(-50%, -50%)' : 'rotate(180deg) translate(-50%, -50%)'};
   padding: 0;
   margin: 0;
   color: ${(props: TProgressBarProgressValue) => props.fillColor};
-  font-size: ${(props: TProgressBarProgressValue) => props.fontSize};
+  font-size: ${(props: TProgressBarProgressValue) => getMeasureValue(props.fontSize)};
   font-family: inherit;
   transform-origin: center;
 `;
