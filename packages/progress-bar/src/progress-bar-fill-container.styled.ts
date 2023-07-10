95b95577b94fb85aaa0bfb3d890dc422764a0eda
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2023-07-10 12:51:33 +0300

    The progressbar component some improved

--- packages/progress-bar/src/progress-bar-fill-container.styled.ts
+++ packages/progress-bar/src/progress-bar-fill-container.styled.ts
@@ -1,14 +1,15 @@
-import styled from "styled-components";
-import TProgressBar from "../types/tprogress-bar";
-import TProgressBarFillContainer from "../types/tprogress-bar-fill-container";
+import styled from 'styled-components';
+import TProgressBar from '../types/tprogress-bar';
+import TProgressBarFillContainer from '../types/tprogress-bar-fill-container';
+import getMeasureValue from '../../helpers/get-measure-value';
 
 const ProgressBarFillContainerStyled = styled.div<TProgressBarFillContainer>`
   height: 100%;
+  box-sizing: border-box;
   width: ${(props: TProgressBarFillContainer) => props.progress}%;
   background: ${(props: TProgressBarFillContainer) => props.fillColor};
-  border-radius: ${(props: TProgressBarFillContainer) => props.borderRadius};
-  transition: ${(props: TProgressBarFillContainer) =>
-    props.isAnimate ? "width 900ms ease-in-out 0s" : "none"};
+  border-radius: ${(props: TProgressBarFillContainer) => getMeasureValue(props.borderRadius, '0')};
+  transition: ${(props: TProgressBarFillContainer) => (props.isAnimate ? 'width 900ms ease-in-out 0s' : 'none')};
 `;
 
 export default ProgressBarFillContainerStyled;
--- packages/progress-bar/src/progress-value.styled.ts
+++ packages/progress-bar/src/progress-value.styled.ts
@@ -1,15 +1,12 @@
 import styled from 'styled-components';
 import TProgressBarProgressValue from '../types/tprogress-bar-progress-value';
 
-const ProgressValueStyled =
-  styled.p <
-  TProgressBarProgressValue >
-  `
+const ProgressValueStyled = styled.p<TProgressBarProgressValue>`
   position: absolute;
   left: 50%;
   top: 50%;
   transform: ${(props: TProgressBarProgressValue) =>
-    props.direction == 'ltr' ? 'translate(-50%, -50%)' : 'rotate(180deg) translateY(50%)'};
+    props.direction == 'ltr' ? 'translate(-50%, -50%)' : 'rotate(180deg) translate(-50%, -50%)'};
   padding: 0;
   margin: 0;
   color: ${(props: TProgressBarProgressValue) => props.fillColor};
