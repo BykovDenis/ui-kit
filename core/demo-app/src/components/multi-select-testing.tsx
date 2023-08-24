517ea19077752d869967c2c93312caf180a223b9
bykov.d.sta@sberbank.ru <bykov.d.sta@sberbank.ru>
2023-08-24 17:23:06 +0300

    The variant atlas added into the MultiSelect

--- core/demo-app/src/components/multi-select-testing.tsx
+++ core/demo-app/src/components/multi-select-testing.tsx
@@ -1,10 +1,11 @@
-import FlexContainer from "@sber-risks-ui/FlexContainer";
 import React, { useState } from "react";
 import MultiSelect from "../../../packages/multi-select/src";
 import TMultiSelectOption from "@sber-risks-ui/MultiSelect/types/tmulti-select-option";
 import GridContainer from "@sber-risks-ui/GridContainer";
+import FlexContainer from "@sber-risks-ui/FormControl";
 
 const MultiSelectTesting: React.FunctionComponent = () => {
+  const [columnsSelected0, setColumnNamesSelected0] = useState<Array<TMultiSelectOption> | []>([]);
   const [columnsSelected1, setColumnNamesSelected1] = useState<Array<TMultiSelectOption> | []>([]);
   const [columnsSelected2, setColumnNamesSelected2] = useState<Array<TMultiSelectOption> | []>([]);
   const [columnsSelected3, setColumnNamesSelected3] = useState<Array<TMultiSelectOption> | []>([]);
@@ -19,6 +20,11 @@ const MultiSelectTesting: React.FunctionComponent = () => {
   ];
 
   const columns1: Array<string> = ['1', '5', '7', '9', '4'];
+  const columns2: Array<string> = ['one', 'two', 'three', 'four', 'five', 'six'];
+
+  const onMultiSelect0Change = (columnNames: Array<TMultiSelectOption>) => {
+    setColumnNamesSelected0(columnNames)
+  }
 
   const onMultiSelect1Change = (columnNames: Array<TMultiSelectOption>) => {
     setColumnNamesSelected1(columnNames)
@@ -33,11 +39,15 @@ const MultiSelectTesting: React.FunctionComponent = () => {
   }
 
 
-  return <GridContainer columns={2} rows={2} gap="40px" padding="50px">
-    <MultiSelect sortDirection="asc" height={100} isUseLocaleStorage={true} id="multi-select-1" label="some-label" name="someColumns1" elementNamesDefaultSelected={columnsSelected1} elementNames={columns} onChange={onMultiSelect1Change}  />
-    <MultiSelect sortDirection="asc" height={100}  isUseLocaleStorage={true} id="multi-select-2" label="some-label" name="someColumns2" elementNamesDefaultSelected={columnsSelected2} elementNames={columns} onChange={onMultiSelect2Change}  />
-    <MultiSelect sortDirection="asc" height={100}  isUseLocaleStorage={true} id="multi-select-3" label="some-label" name="someColumns3" elementNamesDefaultSelected={columnsSelected3} elementNames={columns1} onChange={onMultiSelect3Change}  />
-  </GridContainer>
+  return <>
+    <FlexContainer padding="40px" width={300}>
+      <MultiSelect variant="atlas" sortDirection="asc" height={30} isUseLocaleStorage={false} id="multi-select-1" label="multi-select-atlas" name="someColumns1" elementNamesDefaultSelected={columnsSelected1} elementNames={columns} onChange={onMultiSelect1Change}  />
+    </FlexContainer>
+  {/*  <GridContainer columns={2} rows={2} gap="40px" padding="50px">*/}
+  {/*  <MultiSelect sortDirection="asc" height={100}  isUseLocaleStorage={true} id="multi-select-2" label="some-label" name="someColumns2" elementNamesDefaultSelected={columnsSelected2} elementNames={columns} onChange={onMultiSelect2Change}  />*/}
+  {/*  <MultiSelect sortDirection="asc" height={100}  isUseLocaleStorage={true} id="multi-select-3" label="some-label" name="someColumns3" elementNamesDefaultSelected={columnsSelected3} elementNames={columns1} onChange={onMultiSelect3Change}  />*/}
+  {/*</GridContainer>*/}
+  </>
 }
 
 export default MultiSelectTesting;
--- core/packages/multi-select/src/index.d.ts
+++ core/packages/multi-select/src/index.d.ts
@@ -1,5 +1,6 @@
 import TMultiSelect from "../types/tmulti-select";
 export * as TMultiSelectOption from '../types/tmulti-select-option';
+export * as MultiSelectVariant from '../enums/multi-select-variant';
 
 declare const MultiSelect: React.FunctionComponent<TMultiSelect>;
 export default MultiSelect;
