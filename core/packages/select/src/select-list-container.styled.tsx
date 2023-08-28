69941774a768d058193c7be6d80e6f86b9e1be28
bykov.d.sta@sberbank.ru <bykov.d.sta@sberbank.ru>
2023-08-28 12:57:55 +0300

    The margin-top removed from select items list

--- core/packages/select/src/select-list-container.styled.tsx
+++ core/packages/select/src/select-list-container.styled.tsx
@@ -12,7 +12,7 @@ const SelectListContainer = styled('div')<TSelectListContainer>`
   border-radius: 4px;
   box-shadow: 0px 0px 0px 1px ${props.outlinedColor}, 0px 4px 11px ${props.outlinedColor};
   margin-bottom: 5px;
-  margin-top: 5px;
+  margin-top: -4px;
   position: absolute;
   width: 100%;
   z-index: 10001;
