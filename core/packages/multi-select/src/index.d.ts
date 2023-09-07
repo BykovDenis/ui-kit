bfdaf473c44e341e53b76071c42281e15476ad07
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2023-09-07 12:05:02 +0300

    config edited

--- core/packages/multi-select/src/index.d.ts
+++ core/packages/multi-select/src/index.d.ts
@@ -1,4 +1,7 @@
 import TMultiSelect from '../types/tmulti-select';
+import TMultiSelectOption from '../types/tmulti-select-option';
+import MultiSelectVariant from '../enums/multi-select-variant';
 
 declare const MultiSelect: React.FunctionComponent<TMultiSelect>;
 export default MultiSelect;
+export {TMultiSelectOption, MultiSelectVariant};
