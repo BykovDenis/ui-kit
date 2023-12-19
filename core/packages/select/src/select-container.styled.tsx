61442fff38ece42810cd09879a8c0b64353ae29e
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2023-12-19 17:52:23 +0300

    Versions updated

--- core/packages/select/src/select-container.styled.tsx
+++ core/packages/select/src/select-container.styled.tsx
@@ -1,21 +1,18 @@
 import styled from 'styled-components';
-import getMeasureValue from "../../helpers/get-measure-value";
+import getMeasureValue from '../../helpers/get-measure-value';
 
 interface ISelectContainer {
-  width?: number;
+  width?: number | string;
   height?: number;
   margin: string;
 }
 
-const SelectContainer =
-  styled('div') <
-  ISelectContainer >
-  `
+const SelectContainer = styled('div')<ISelectContainer>`
   position: relative;
   display: block;
-  margin:  ${(props: ISelectContainer) => props?.margin ?? 0};
+  margin: ${(props: ISelectContainer) => props?.margin ?? 0};
   padding: 0;
-  width: ${(props: ISelectContainer) => (props?.width ? `${props?.width}px` : `100%`)};
+  width: ${(props: ISelectContainer) => getMeasureValue(props.width, '100%')};
   height: ${(props: ISelectContainer) => (props?.height ? `${props?.height + 5}px` : 'calc(100% + 5px)')};
   line-height: 1;
 `;
