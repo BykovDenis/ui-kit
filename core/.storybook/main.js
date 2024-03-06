f85180301a8e5fbbccfbc8967ef246ac80f2c0dd
bykov.d.sta@sberbank.ru <bykov.d.sta@sberbank.ru>
2024-03-06 14:02:55 +0300

    Storybook config returned

---
+++ core/.storybook/main.js
@@ -0,0 +1,19 @@
+import { mergeConfig } from 'vite';
+
+export default {
+  framework: '@storybook/react-vite',
+  stories: ['../packages/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      // Add dependencies to pre-optimization
+      optimizeDeps: {
+        include: ['storybook-dark-mode'],
+      },
+    });
+  },
+};
