754c841ab8577c727b5b9fd7d83649507f932986
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2024-04-10 15:44:15 +0300

    Build sh script added for building without lerna

---
+++ core/build.sh
@@ -0,0 +1,117 @@
+cd packages
+cd button/
+echo 1. Button building...
+npm run build
+cd ..
+cd checkbox/
+echo 2. Checkbox building...
+npm run build
+cd ..
+cd divider/
+echo 3. Divider building...
+npm run build
+cd ..
+cd file-uploader/
+echo 4. File uploader building...
+npm run build
+cd ..
+cd flex-container/
+echo 5. FlexContainer building...
+npm run build
+cd ..
+cd form-control/
+echo 6. FormControl building...
+npm run build
+cd ..
+cd grid-container/
+echo 7. FridContainer building...
+npm run build
+cd ..
+cd icon-button/
+echo 8. IconButton building...
+npm run build
+cd ..
+cd input/
+echo 9. Input building...
+npm run build
+cd ..
+cd label/
+echo 10. Label building...
+npm run build
+cd ..
+cd label-interactive/
+echo 11. LabelInteractive building...
+npm run build
+cd ..
+cd list/
+echo 12. List building...
+npm run build
+cd ..
+cd list-item/
+echo 13. ListItem building...
+npm run build
+cd ..
+cd multi-select/
+echo 14. MultiSelect building...
+npm run build
+cd ..
+cd progress-bar/
+echo 15. ProgressBar building...
+npm run build
+cd ..
+cd radio/
+echo 16. Radio building...
+npm run build
+cd ..
+cd select/
+echo 17. Select building...
+npm run build
+cd ..
+cd switcher/
+echo 18. Switcher building...
+npm run build
+cd ..
+cd tab/
+echo 19. Tab building...
+npm run build
+cd ..
+cd table/
+echo 20. Table building...
+npm run build
+cd ..
+cd table-body/
+echo 21. TableBody building...
+npm run build
+cd ..
+cd table-cell/
+echo 22. TableCell building...
+npm run build
+cd ..
+cd table-columns-visible/
+echo 23. TableColumnsVisible building...
+npm run build
+cd ..
+cd table-head/
+echo 24. TableHead building...
+npm run build
+cd ..
+cd table-row/
+echo 25. TableRow building...
+npm run build
+cd ..
+cd tabs/
+echo 26. Tabs building...
+npm run build
+cd ..
+cd textfield/
+echo 27. TextField building...
+npm run build
+cd ..
+cd typography/
+echo 28. Typography building...
+npm run build
+cd ..
+cd warning-panel/
+echo 29. WarningPanel building...
+npm run build
+cd ..
