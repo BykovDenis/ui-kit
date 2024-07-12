377bf8f51cbc91e2442b7e7e0989a2dadc5ab816
Denis Bykov Stanislavovich <bykov.d.sta@sberbank.ru>
2024-07-12 11:14:45 +0300

    PSCVA-1155 Some refactoring

--- core/demo-app/src/app.tsx
+++ core/demo-app/src/app.tsx
@@ -1,20 +1,8 @@
 import { useContext } from 'react';
-import getNewReactThemeContext from '../../packages/styles';
 import themeDark from './themes/theme-dark';
-import InputTesting from './components/input-testing';
-import TextFieldTesting from './components/text-field-testing';
-// import Select from '../../packages/select';
-import SelectTesting from './components/select-testing';
-// import GridContainer from '../../packages/grid-container';
-// import GridContainerTesting from './components/grid-container-testing';
-// import MultiSelectTesting from './components/multi-select-testing';
-import DatepickerTesting from './components/datepicker-testing';
 import Sidebar from './components/sidebar';
-// import JsonToTable from './components/json-to-table';
-// import ReactContext from './components/react-context';
-// import FlexContainerTesting from './components/flex-container-testing';
-// import FileUploaderTesting from './components/file-uploader-testing';
 import GridContainer from '../../packages/grid-container';
+import getNewReactThemeContext from "@sber-risks-ui/core/styles";
 
 type AppProps = {
   children: React.ReactNode;
@@ -32,16 +20,6 @@ const App: React.FunctionComponent<AppProps> = (props: AppProps) => {
           <Sidebar />
           <div style={{ background: context.mainBackgroundColor, height: '100vh', margin: 0 }}>
             {props.children}
-            {/*<ReactContext />*/}
-            {/*<JsonToTable />*/}
-            {/*<DatepickerTesting />*/}
-            {/*<SelectTesting />*/}
-            {/*<InputTesting />*/}
-            {/*<TextFieldTestin g />*/}
-            {/*<GridContainerTesting />*/}
-            {/*<MultiSelectTesting />*/}
-            {/*<FlexContainerTesting />*/}
-            {/*<FileUploaderTesting />*/}
           </div>
         </GridContainer>
       </ReactThemeContext.Provider>
