bc9ac704ac972e02c1415434d0ca7f489ac06205
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2024-07-19 13:00:39 +0300

    PSCVA-1155 Datepicker some improved

--- core/cypress/e2e/datepicker.cy.tsx
+++ core/cypress/e2e/datepicker.cy.tsx
@@ -4,7 +4,7 @@ describe('The Datepicker component', () => {
     cy.viewport(1920, 1080);
     // cy.get('button[data-test="btn-delete-value"]').as('btn').click({ multiple: true});
   });
-  it('Test1. Selection by click text. Testing input value changing attributes by onChange event handler. Mask dd.MM.yyyy (onChange)', () => {
+  it('Test 1. Selection by click text. Testing input value changing attributes by onChange event handler. Mask dd.MM.yyyy (onChange)', () => {
     // Typing value
     cy.get('#datepicker1').click().type('01042024').blur().should('value', '01.04.2024');
     // Remove values
@@ -12,14 +12,14 @@ describe('The Datepicker component', () => {
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click({ multiple: true });
     cy.get('#datepicker1').should('value', '');
   });
-  it('Test2. Selection by click text. Testing input value changing attributes by onChange event handler. Mask yyyy-MM-dd (onBlur)', () => {
+  it('Test 2. Selection by click text. Testing input value changing attributes by onChange event handler. Mask yyyy-MM-dd (onBlur)', () => {
     // Typing value
     cy.get('#datepicker2').click().type('20240401').blur().should('value', '2024-04-01');
     // Remove values
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click({ multiple: true });
     cy.get('#datepicker2').should('value', '');
   });
-  it('Test3. Selection by click. Testing value changing attributes by onChange event handler. Mask dd.MM.yyyy  (onChange)', () => {
+  it('Test 3. Selection by click. Testing value changing attributes by onChange event handler. Mask dd.MM.yyyy  (onChange)', () => {
     cy.get('#datepicker1').focus().type('01.04.2024').blur().should('value', '01.04.2024');
     cy.get('#datepicker1').focus().click();
     cy.get('button[name="10"]').as('btn').click();
@@ -28,7 +28,7 @@ describe('The Datepicker component', () => {
     cy.get('button[name="23"]').as('btn').click();
     cy.get('#datepicker1').should('value', '23.04.2024');
   });
-  it('Test4. Selection by click. Testing value changing attributes by onChange event handler. Mask yyyy-MM-dd (onBlur)', () => {
+  it('Test 4. Selection by click. Testing value changing attributes by onChange event handler. Mask yyyy-MM-dd (onBlur)', () => {
     cy.get('#datepicker2').focus().type('20240201').blur().should('value', '2024-02-01');
     cy.get('#datepicker2').focus().click();
     cy.get('button[name="28"]').as('btn').click();
@@ -37,45 +37,45 @@ describe('The Datepicker component', () => {
     cy.get('button[name="13"]').as('btn').click();
     cy.get('#datepicker2').should('value', '2024-02-13');
   });
-  it('Test5. Test min-max date  (onChange)', () => {
+  it('Test 5. Test min-max date  (onChange)', () => {
     cy.get('#datepicker3').focus().click();
     cy.get('button[data-test-name="setToday"]').should('be.disabled');
   });
-  it('Test6. Selection by click text after invalid typing value  (onChange)', () => {
+  it('Test 6. Selection by click text after invalid typing value  (onChange)', () => {
     // Typing value
-    cy.get('#datepicker1').focus().type('03042024').blur().should('value', '03042024');
+    cy.get('#datepicker1').focus().type('03042024').blur().should('value', '03.04.2024');
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click();
-    cy.get('#datepicker1').focus().type('55032024').blur().should('value', '55032024');
+    cy.get('#datepicker1').focus().type('55032024').blur().should('value', '55.03.2024');
     cy.get('[data-test-id=datepicker1-text-message]').should('have.text', 'Дата не валидна');
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click({ multiple: true });
     cy.get('#datepicker1').should('value', '');
   });
-  it('Test7. Selection by click text after invalid typing value1 and checking invalid typing value (onBlur)', () => {
+  it('Test 7. Selection by click text after invalid typing value1 and checking invalid typing value (onBlur)', () => {
     cy.get('#datepicker2').focus().type('2024-04-03').blur().should('value', '2024-04-03');
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click();
     cy.get('#datepicker2').focus().type('2024-03-54').blur().should('value', '2024-03-54');
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click({ multiple: true });
     cy.get('#datepicker2').should('value', '');
   });
-  it('Test8. Input invalid date event handler onChange  (onChange)', () => {
+  it('Test 8. Input invalid date event handler onChange  (onChange)', () => {
     cy.get('#datepicker1').focus().type('03.04.2024').blur().should('value', '03.04.2024');
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click();
-    cy.get('#datepicker1').focus().type('03.04').blur().should('value', '03.04.');
+    cy.get('#datepicker1').focus().type('03.04').blur().should('value', '03.04');
     cy.get('[data-test-id=datepicker1-text-message]').should('have.text', 'Дата не валидна');
   });
-  it('Test9. Input invalid date event handler  (onBlur)', () => {
+  it('Test 9. Input invalid date event handler  (onBlur)', () => {
     cy.get('#datepicker2').focus().type('2024-04-03').blur().should('value', '2024-04-03');
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click();
     cy.get('#datepicker2').focus().type('2024-03-5').blur().should('value', '2024-03-5');
     cy.get('[data-test-id=datepicker2-text-message]').should('have.text', 'Date is not valid');
   });
-  it('Test10. Input invalid date event handler  (onBlur)', () => {
+  it('Test 10. Input invalid date event handler  (onBlur)', () => {
     cy.get('#datepicker2').focus().type('2024-04-03').blur().should('value', '2024-04-03');
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click();
-    cy.get('#datepicker2').focus().type('2024-03').blur().should('value', '2024-03-');
+    cy.get('#datepicker2').focus().type('2024-03').blur().should('value', '2024-03');
     cy.get('[data-test-id=datepicker2-text-message]').should('have.text', 'Date is not valid');
   });
-  it('Test10. Change to previous month  (onBlur)', () => {
+  it('Test 11. Change to previous month  (onBlur)', () => {
     cy.get('#datepicker2').focus().type('2024-04-03').blur().should('value', '2024-04-03');
     cy.get('#datepicker2')
       .focus()
@@ -87,7 +87,7 @@ describe('The Datepicker component', () => {
         cy.get('#datepicker2').should('value', '2024-02-05');
       });
   });
-  it('Test11. Change to next month  (onBlur)', () => {
+  it('Test 12. Change to next month  (onBlur)', () => {
     cy.get('#datepicker2').focus().type('2024-04-03').blur().should('value', '2024-04-03');
     cy.get('#datepicker2')
       .focus()
@@ -99,7 +99,7 @@ describe('The Datepicker component', () => {
         cy.get('#datepicker2').should('value', '2024-06-20');
       });
   });
-  it('Test12. Change to previous year (onBlur)', () => {
+  it('Test 13. Change to previous year (onBlur)', () => {
     cy.get('#datepicker2').focus().type('2024-04-03').blur().should('value', '2024-04-03');
     cy.get('#datepicker2')
       .focus()
@@ -111,7 +111,7 @@ describe('The Datepicker component', () => {
         cy.get('#datepicker2').should('value', '2022-04-05');
       });
   });
-  it('Test13. Change to next year  (onBlur)', () => {
+  it('Test 14. Change to next year  (onBlur)', () => {
     cy.get('#datepicker2').focus().type('2024-04-03').blur().should('value', '2024-04-03');
     cy.get('#datepicker2')
       .focus()
@@ -123,7 +123,7 @@ describe('The Datepicker component', () => {
         cy.get('#datepicker2').should('value', '2026-04-20');
       });
   });
-  it('Test1. Selection by click text. Testing input value changing attributes by onChange event handler. Mask dd.MM.yyyy (onChange)', () => {
+  it('Test 15. Selection by click text. Testing input value changing attributes by onChange event handler. Mask dd.MM.yyyy (onChange)', () => {
     // Typing value
     cy.get('#datepicker1').focus().type('01.04.2024').blur().should('value', '01.04.2024');
     // Remove values
@@ -131,14 +131,14 @@ describe('The Datepicker component', () => {
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click({ multiple: true });
     cy.get('#datepicker1').should('value', '');
   });
-  it('Test2. Selection by click text. Testing input value changing attributes by onChange event handler. Mask yyyy-MM-dd (onBlur)', () => {
+  it('Test 16. Selection by click text. Testing input value changing attributes by onChange event handler. Mask yyyy-MM-dd (onBlur)', () => {
     // Typing value
     cy.get('#datepicker2').focus().type('2024-04-01').blur().should('value', '2024-04-01');
     // Remove values
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click({ multiple: true });
     cy.get('#datepicker2').should('value', '');
   });
-  it('Test3. Selection by click. Testing value changing attributes by onChange event handler. Mask dd.MM.yyyy  (onChange)', () => {
+  it('Test 17. Selection by click. Testing value changing attributes by onChange event handler. Mask dd.MM.yyyy  (onChange)', () => {
     cy.get('#datepicker1').focus().type('01.04.2024').blur().should('value', '01.04.2024');
     cy.get('#datepicker1').focus().click();
     cy.get('button[name="10"]').as('btn').click();
@@ -147,7 +147,7 @@ describe('The Datepicker component', () => {
     cy.get('button[name="23"]').as('btn').click();
     cy.get('#datepicker1').should('value', '23.04.2024');
   });
-  it('Test4. Selection by click. Testing value changing attributes by onChange event handler. Mask yyyy-MM-dd (onBlur)', () => {
+  it('Test 18. Selection by click. Testing value changing attributes by onChange event handler. Mask yyyy-MM-dd (onBlur)', () => {
     cy.get('#datepicker2').focus().type('2024-02-01').blur().should('value', '2024-02-01');
     cy.get('#datepicker2').focus().click();
     cy.get('button[name="28"]').as('btn').click();
@@ -156,11 +156,11 @@ describe('The Datepicker component', () => {
     cy.get('button[name="13"]').as('btn').click();
     cy.get('#datepicker2').should('value', '2024-02-13');
   });
-  it('Test5. Test min-max date  (onChange)', () => {
+  it('Test 19. Test min-max date  (onChange)', () => {
     cy.get('#datepicker3').focus().click();
     cy.get('button[data-test-name="setToday"]').should('be.disabled');
   });
-  it('Test6. Selection by click text after invalid typing value  (onChange)', () => {
+  it('Test 20. Selection by click text after invalid typing value  (onChange)', () => {
     // Typing value
     cy.get('#datepicker1').focus().type('03.04.2024').blur().should('value', '03.04.2024');
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click();
@@ -169,32 +169,32 @@ describe('The Datepicker component', () => {
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click({ multiple: true });
     cy.get('#datepicker1').should('value', '');
   });
-  it('Test7. Selection by click text after invalid typing value1 and checking invalid typing value (onBlur)', () => {
+  it('Test 21. Selection by click text after invalid typing value1 and checking invalid typing value (onBlur)', () => {
     cy.get('#datepicker2').focus().type('2024-04-03').blur().should('value', '2024-04-03');
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click();
     cy.get('#datepicker2').focus().type('2024-03-54').blur().should('value', '2024-03-54');
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click({ multiple: true });
     cy.get('#datepicker2').should('value', '');
   });
-  it('Test8. Input invalid date event handler onChange  (onChange)', () => {
+  it('Test 22. Input invalid date event handler onChange  (onChange)', () => {
     cy.get('#datepicker1').focus().type('03.04.2024').blur().should('value', '03.04.2024');
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click();
-    cy.get('#datepicker1').focus().type('03.04').blur().should('value', '03.04.');
+    cy.get('#datepicker1').focus().type('03.04').blur().should('value', '03.04');
     cy.get('[data-test-id=datepicker1-text-message]').should('have.text', 'Дата не валидна');
   });
-  it('Test9. Input invalid date event handler  (onBlur)', () => {
+  it('Test 23. Input invalid date event handler  (onBlur)', () => {
     cy.get('#datepicker2').focus().type('2024-04-03').blur().should('value', '2024-04-03');
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click();
     cy.get('#datepicker2').focus().type('2024-03-5').blur().should('value', '2024-03-5');
     cy.get('[data-test-id=datepicker2-text-message]').should('have.text', 'Date is not valid');
   });
-  it('Test10. Input invalid date event handler  (onBlur)', () => {
+  it('Test 24. Input invalid date event handler  (onBlur)', () => {
     cy.get('#datepicker2').focus().type('2024-04-03').blur().should('value', '2024-04-03');
     cy.get('button[data-test="btn-delete-value"]').as('btn').parent().click();
-    cy.get('#datepicker2').focus().type('2024-03').blur().should('value', '2024-03-');
+    cy.get('#datepicker2').focus().type('2024-03').blur().should('value', '2024-03');
     cy.get('[data-test-id=datepicker2-text-message]').should('have.text', 'Date is not valid');
   });
-  it('Test10. Change to previous month  (onBlur)', () => {
+  it('Test 25. Change to previous month  (onBlur)', () => {
     cy.get('#datepicker2').focus().type('2024-04-03').blur().should('value', '2024-04-03');
     cy.get('#datepicker2')
       .focus()
@@ -206,7 +206,7 @@ describe('The Datepicker component', () => {
         cy.get('#datepicker2').should('value', '2024-02-05');
       });
   });
-  it('Test11. Change to next month  (onBlur)', () => {
+  it('Test 26. Change to next month  (onBlur)', () => {
     cy.get('#datepicker2').focus().type('2024-04-03').blur().should('value', '2024-04-03');
     cy.get('#datepicker2')
       .focus()
@@ -218,7 +218,7 @@ describe('The Datepicker component', () => {
         cy.get('#datepicker2').should('value', '2024-06-20');
       });
   });
-  it('Test12. Change to previous year (onBlur)', () => {
+  it('Test 27. Change to previous year (onBlur)', () => {
     cy.get('#datepicker2').focus().type('2024-04-03').blur().should('value', '2024-04-03');
     cy.get('#datepicker2')
       .focus()
@@ -230,7 +230,7 @@ describe('The Datepicker component', () => {
         cy.get('#datepicker2').should('value', '2022-04-05');
       });
   });
-  it('Test13. Change to next year  (onBlur)', () => {
+  it('Test 28. Change to next year  (onBlur)', () => {
     cy.get('#datepicker2').focus().type('2024-04-03').blur().should('value', '2024-04-03');
     cy.get('#datepicker2')
       .focus()
@@ -242,7 +242,7 @@ describe('The Datepicker component', () => {
         cy.get('#datepicker2').should('value', '2026-04-20');
       });
   });
-  it('Test14. Change month and year from dropdown (onChange)', () => {
+  it('Test 29. Change month and year from dropdown (onChange)', () => {
     cy.get('#datepicker1').focus().type('03.04.2024').blur().should('value', '03.04.2024');
     cy.get('#datepicker1').focus().click();
     cy.get('#datepicker-month-datepicker1-input').click().focus();
@@ -252,7 +252,7 @@ describe('The Datepicker component', () => {
     cy.get('button[data-value="2025"]').as('btn').click();
     cy.get('#datepicker1').should('value', '03.09.2025');
   });
-  it('Test15. Change month and year from dropdown (onBlur)', () => {
+  it('Test 30. Change month and year from dropdown (onBlur)', () => {
     cy.get('#datepicker2').focus().type('2024-05-19').blur().should('value', '2024-05-19');
     cy.get('#datepicker2').focus().click();
     cy.get('#datepicker-month-datepicker2-input').click().focus();
@@ -262,7 +262,7 @@ describe('The Datepicker component', () => {
     cy.get('button[data-value="2028"]').as('btn').click();
     cy.get('#datepicker2').should('value', '2028-09-19');
   });
-  it('Test16. Delete a date digit without resetting the cursor position dd.MM.yyyy', () => {
+  it('Test 31. Delete a date digit without resetting the cursor position dd.MM.yyyy', () => {
     cy.get('#datepicker2').focus().type('2024-02-19').blur().should('value', '2024-02-19');
     cy.get('#datepicker2')
       .focus()
@@ -270,7 +270,7 @@ describe('The Datepicker component', () => {
       .blur()
       .should('value', '2024-12-19');
   });
-  it('Test17. Delete a date digit without resetting the cursor position yyyy.MM.dd', () => {
+  it('Test 32. Delete a date digit without resetting the cursor position yyyy.MM.dd', () => {
     cy.get('#datepicker1').focus().type('19.02.2024').blur().should('value', '19.02.2024');
     cy.get('#datepicker1')
       .focus()
--- core/demo-app/src/components/file-uploader-testing.tsx
+++ core/demo-app/src/components/file-uploader-testing.tsx
@@ -1,5 +1,5 @@
 import FlexContainer from '@sber-risks-ui/core/flex-container';
-import FileUploader from '@sber-risks-ui/core/file-uploader';
+// import FileUploader from '@sber-risks-ui/core/file-uploader';
 
 const FileUploaderTesting: React.FunctionComponent = () => {
   const onFileUpload = (files: Array<File>, evt: any) => {
@@ -10,24 +10,25 @@ const FileUploaderTesting: React.FunctionComponent = () => {
     <>
       <FlexContainer flexDirection="column" alignItems="ceter" justifyContent="center">
         <FlexContainer>
-          <FileUploader
-            width={56}
-            height={46}
-            id="some-some-uploader"
-            disabled={false}
-            onFileUpload={onFileUpload}
-            isMultiple={true}
-          >
-            <svg
-              className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv"
-              focusable="false"
-              aria-hidden="true"
-              viewBox="0 0 24 24"
-              data-testid="PublishIcon"
-            >
-              <path d="M5 4v2h14V4H5zm0 10h4v6h6v-6h4l-7-7-7 7z"></path>
-            </svg>
-          </FileUploader>
+          File uploader
+          {/*<FileUploader*/}
+          {/*  width={56}*/}
+          {/*  height={46}*/}
+          {/*  id="some-some-uploader"*/}
+          {/*  disabled={false}*/}
+          {/*  onFileUpload={onFileUpload}*/}
+          {/*  isMultiple={true}*/}
+          {/*>*/}
+          {/*  <svg*/}
+          {/*    className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv"*/}
+          {/*    focusable="false"*/}
+          {/*    aria-hidden="true"*/}
+          {/*    viewBox="0 0 24 24"*/}
+          {/*    data-testid="PublishIcon"*/}
+          {/*  >*/}
+          {/*    <path d="M5 4v2h14V4H5zm0 10h4v6h6v-6h4l-7-7-7 7z"></path>*/}
+          {/*  </svg>*/}
+          {/*</FileUploader>*/}
         </FlexContainer>
       </FlexContainer>
     </>
