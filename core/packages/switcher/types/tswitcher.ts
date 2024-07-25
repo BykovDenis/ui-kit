42da42d339932cc2ca16220f41f01772c9d917fb
16783656 <bykov.d.sta@sberbank.ru>
2024-07-25 18:32:20 +0300

    CIBFV-4163 Update event handler Switcher component

--- core/packages/switcher/types/tswitcher.ts
+++ core/packages/switcher/types/tswitcher.ts
@@ -1,17 +1,20 @@
-import CSS from "csstype";
+import CSS from 'csstype';
+import React from 'react';
 
 type TSwitcher = {
-  activeElement?: string | undefined,
-  backgroundColor?: CSS.Property.BackgroundColor,
-  classes?: any,
-  color?: CSS.Property.Color,
-  disabled?: boolean,
-  element1: string,
-  element2: string,
-  onSwitcherChange?: (value: string) => void,
-  height?: number | string,
-  width?: number | string,
-  fontSize?: number | string,
+  id?: string;
+  name?: string;
+  activeElement?: string | undefined;
+  backgroundColor?: CSS.Property.BackgroundColor;
+  classes?: any;
+  color?: CSS.Property.Color;
+  disabled?: boolean;
+  element1: string;
+  element2: string;
+  onSwitcherChange?: (value: string, name?: string, evt?: React.ChangeEvent<HTMLInputElement>) => void;
+  height?: number | string;
+  width?: number | string;
+  fontSize?: number | string;
 };
 
 export default TSwitcher;
