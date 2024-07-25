079936ac3b9d444b69b2e76add6c47e8759805dd
Bykov Denis Stanislavovich <bykov_denis@icloud.com>
2024-07-25 18:06:48 +0300

    CIBFV-4163 Change types

--- core/packages/form-control/types/iform-control-styled.d.ts
+++
@@ -1,8 +0,0 @@
-import * as CSS from 'csstype';
-import TBaseStyles from '../../types/tbase-styles';
-
-interface IFormControlStyled extends TBaseStyles {
-  fontSize?: number | string;
-}
-
-export default IFormControlStyled;
