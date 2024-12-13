8a797b33891b588feca15eb2c530f7cbcad1ec5d
bykov.d.sta@sberbank.ru <bykov.d.sta@sberbank.ru>
2024-12-13 16:54:56 +0300

    CIBFV-4493 Multiselect. Add e2e tests

---
+++ core/cypress/e2e/multiselect.cy.tsx
@@ -0,0 +1,35 @@
+describe('Testing MultiSelect component', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/multi-select');
+    cy.viewport(1920, 1080);
+  });
+  it('Test 1. MultiSelect. Select elements', () => {
+    cy.get('[data-cy="multi-select-2-btn-expand"]')
+      .click()
+      .then(() => {
+        cy.get('[data-cy="multi-select-2-search-input"]').click().type('B1');
+        cy.get('[data-label="B1_SH_UK"]').click();
+        cy.get('[data-label="B1_OL_UK"]').click();
+        cy.get('[data-label="B1_SC_UK"]').click();
+        cy.get('[data-label="B1_SS1_UK"]')
+          .click()
+          .then(() => {
+            cy.get('[data-cy="multi-select-2-selected-elements"]').find('div').should('have.length', 4);
+            cy.get('#multi-select-2-2-button').click();
+            cy.get('[data-cy="multi-select-2-selected-elements"]').find('div').should('have.length', 3);
+          });
+      });
+  });
+  it('Test 2. MultiSelect. Disable elements', () => {
+    cy.get('#selected-metrics-objects')
+      .invoke('removeAttr', 'disabled')
+      .find('[data-cy="selected-metrics-objects-toggle-container"]')
+      .should('have.length', 0);
+  });
+  it('Test 3. MultiSelect. Disable elements', () => {
+    cy.get('#multi-select-strings')
+      .invoke('removeAttr', 'disabled')
+      .find('[data-cy="multi-select-strings-toggle-container"]')
+      .should('have.length', 0);
+  });
+});
