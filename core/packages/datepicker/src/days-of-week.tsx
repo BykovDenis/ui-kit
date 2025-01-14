be5bc10f5ba413d48a6c09000442dc47502c7dd8
bykov.d.sta@sberbank.ru <bykov.d.sta@sberbank.ru>
2025-01-14 16:49:43 +0300

    CIBFV-4218 Improve datepicker

--- core/packages/datepicker/src/days-of-week.tsx
+++ core/packages/datepicker/src/days-of-week.tsx
@@ -8,16 +8,23 @@ import daysOfWeekEn from '../dictionaries/days-of-week-en';
 import daysOfWeekRu from '../dictionaries/days-of-week-ru';
 import IDayOfWeek from '../types/iday-of-week';
 import DayOfWeek from './day-of-week.styled';
+import UiKitComponent from '../../enums/ui-kit-component';
 
 const DaysOfWeek: React.FunctionComponent<IDayOfWeek> = (props: IDayOfWeek) => {
   const daysOfWeekElements: Array<string> = props.locale === Locale.Ru ? daysOfWeekRu : daysOfWeekEn;
   return (
-    <List className="list" fontFamily={props.fontFamily} width={230}>
+    <List data-ui-kit-component={UiKitComponent.Datepicker} className="list" fontFamily={props.fontFamily} width={230}>
       {daysOfWeekElements.map((element: string, index: number) => {
         const color: string = index < 5 ? props.primaryColor : props.secondaryColor;
 
         return (
-          <DayOfWeek color={color} fontSize={props.fontSize - 2} fontFamily={props.fontFamily} key={element}>
+          <DayOfWeek
+            data-ui-kit-component={UiKitComponent.Datepicker}
+            color={color}
+            fontSize={props.fontSize - 2}
+            fontFamily={props.fontFamily}
+            key={element}
+          >
             {element}
           </DayOfWeek>
         );
