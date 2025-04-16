63fccd72b1c44e9e7eb60cc509bb2937ba27c2ff
Быков Денис Станиславович <Bykov.D.Sta@sberbank.ru>
2025-04-16 17:05:24 +0300

    CIBFV-4491 Fix focus input

--- core/cypress/e2e/multiselect.cy.tsx
+++ core/cypress/e2e/multiselect.cy.tsx
@@ -11,10 +11,10 @@ describe('Testing MultiSelect component', () => {
         cy.get('#multi-select-2').find('button[data-label="B1_OL_UK"]').click();
         cy.get('#multi-select-2').find('button[data-label="B1_SC_UK"]').click();
         cy.get('[data-cy="multi-select-2-selected-elements"]').find('div').should('have.length', 1);
-        cy.get('#multi-select-2').find('[data-cy="multi-select-2-search-input"]').click().type('B1');
+        cy.get('[data-cy="multi-select-2-search-input"]').click().type('B1');
         cy.get('[data-cy="multi-select-2-list"]').find('button[data-label="B1_SH_UK"]').click();
-        cy.get('#multi-select-2').find('[data-label="B1_OL_UK"]').click();
-        cy.get('#multi-select-2').find('[data-label="B1_SC_UK"]').click();
+        cy.get('[data-cy="multi-select-2-list"]').find('[data-label="B1_OL_UK"]').click();
+        cy.get('[data-cy="multi-select-2-list"]').find('[data-label="B1_SC_UK"]').click();
         cy.get('[data-cy="multi-select-2-selected-elements"]').find('div').should('have.length', 4);
         cy.get('#multi-select-2-2-button').click();
         cy.get('[data-cy="multi-select-2-selected-elements"]').find('div').should('have.length', 3);
